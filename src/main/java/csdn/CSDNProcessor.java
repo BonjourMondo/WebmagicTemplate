027e863034757699198f3b8841dd3aa0b17387f1
好大一条鱼 <jkusjkus89757@163.com>
2019-02-22 16:06:51 +0800

    完善csdn以及Porn

--- src/main/java/csdn/CSDNProcessor.java
+++ src/main/java/csdn/CSDNProcessor.java
@@ -43,7 +43,7 @@ public class CSDNProcessor implements PageProcessor {
 
     public Site getSite() {
         if (site==null) {
-            site=Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000)
+            site=Site.me().setRetryTimes(3).setSleepTime(500).setTimeOut(10000)
                     .setDomain("blog.csdn.net").addCookie("blog.csdn.net", "__yadk_uid", "Qm7dEc8XUauSwGnL8pAdY60k4lzp8JVv")
                     .addHeader("referer","https://blog.csdn.net/No_Game_No_Life_")
                     .setCharset("utf-8").setUserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3633.400 QQBrowser/10.4.3232.400");
@@ -61,16 +61,22 @@ public class CSDNProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        long startTime, endTime;
-        System.out.println("开始爬取...");
-        startTime = System.currentTimeMillis();
-        Spider.create(new CSDNProcessor())
-                .setScheduler(new PriorityScheduler())
-                .addPipeline(new FilePipeline("D:\\英雄时刻"))
-                .addUrl("https://blog.csdn.net/No_Game_No_Life_/")
-                .thread(2).run();
-        endTime = System.currentTimeMillis();
-        System.out.println("爬取结束，耗时约" + ((endTime - startTime) / 1000) + "秒，抓取了"+count+"条记录");
-
+        while (true) {
+            long startTime, endTime;
+            System.out.println("开始爬取...");
+            startTime = System.currentTimeMillis();
+            Spider.create(new CSDNProcessor())
+                    .setScheduler(new PriorityScheduler())
+                    .addPipeline(new ConsolePipeline())
+                    .addUrl("https://blog.csdn.net/No_Game_No_Life_/")
+                    .thread(2).run();
+            endTime = System.currentTimeMillis();
+            System.out.println("爬取结束，耗时约" + ((endTime - startTime) / 1000) + "秒，抓取了" + count + "条记录");
+            try {
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/porn/PornProcessor.java
+++ src/main/java/porn/PornProcessor.java
@@ -1,3 +1,5 @@
+package porn;
+
 import csdn.CSDNProcessor;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -8,38 +10,40 @@ import us.codecraft.webmagic.scheduler.PriorityScheduler;
 
 /**
  * Author: LeesangHyuk
- * Date: 2019/2/21 10:49
- * Description:爬虫模版
+ * Date: 2019/2/21 19:12
+ * Description:
  */
-public class TemplateProcessor implements PageProcessor {
+public class PornProcessor implements PageProcessor {
 
     private Site site;
     private static int count=0;
 
-    public static final String URL_LIST = "";
-    public static final String URL_POST = "";
+    public static final String URL_LIST1 = "https://www.752ii.com/shipin/nvyou.*.html";
+    public static final String URL_LIST12="https://www.752ii.com/shipin/list.*.html";
+    public static final String URL_POST = "https://www.752ii.com/shipin/[0-9]*.html";
 
     public void process(Page page) {
         if (page.getUrl().regex(URL_POST).match()) {
-            page.putField("A",page.getHtml().xpath(""));
-            if (page.getResultItems().get("A") == null) {
+            page.putField("片名",page.getHtml().xpath("//*[@id=\"shipin-detail-content-pull\"]/div[2]/div[1]/h2/text()"));
+            if (page.getResultItems().get("片名") == null) {
                 page.setSkip(true);
             }else {
                 count++;
             }
-            page.putField("url",page.getUrl().toString());
-
+//            page.putField("url",page.getUrl().toString());
+            page.putField("actor",page.getHtml().xpath("//*[@id=\"shipin-detail-content-pull\"]/div[2]/div[3]/p/text()"));
+            page.putField("下载链接",page.getHtml().xpath("//*[@id=\"lin1k0\"]/@value"));
         }else{
             page.addTargetRequests(page.getHtml().links().regex(URL_POST).all(), 1000);
-            page.addTargetRequests(page.getHtml().links().regex(URL_LIST).all(), 1);
+            page.addTargetRequests(page.getHtml().links().regex(URL_LIST1).all(), 1);
+            page.addTargetRequests(page.getHtml().links().regex(URL_LIST12).all(),1);
         }
     }
 
     public Site getSite() {
         if (site==null) {
             site=Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000)
-                    .setDomain("blog.csdn.net").addCookie("blog.csdn.net", "__yadk_uid", "Qm7dEc8XUauSwGnL8pAdY60k4lzp8JVv")
-                    .addHeader("referer","https://blog.csdn.net/No_Game_No_Life_")
+                    .addHeader("referer","ttps://www.752ii.com/shipin/nvyou-julia-3.html")
                     .setCharset("utf-8").setUserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3633.400 QQBrowser/10.4.3232.400");
 
         }
@@ -50,10 +54,10 @@ public class TemplateProcessor implements PageProcessor {
         long startTime, endTime;
         System.out.println("开始爬取...");
         startTime = System.currentTimeMillis();
-        Spider.create(new TemplateProcessor())
+        Spider.create(new PornProcessor())
                 .setScheduler(new PriorityScheduler())
                 .addPipeline(new ConsolePipeline())
-                .addUrl("https://blog.csdn.net/No_Game_No_Life_/")
+                .addUrl("https://www.752ii.com/shipin/list-%E5%A5%B3%E4%BC%98%E4%B8%93%E8%BE%91-1.html")
                 .thread(5).run();
         endTime = System.currentTimeMillis();
         System.out.println("爬取结束，耗时约" + ((endTime - startTime) / 1000) + "秒，抓取了"+count+"条记录");
